954eb3e868be9a8999f95d4c59a6e142f3eb1ce3
jsavage5 <jamessavage1990@gmail.com>
2024-10-13 07:49:12 -0500

    better black screen

--- main.py
+++ main.py
@@ -2,22 +2,26 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
 
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-          return
-    pygame.Surface.fill(screen, (0, 0, 0))
-    pygame.display.flip()
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
-  print("Starting asteroids!")
-  print(f"Screen width: {SCREEN_WIDTH}")
-  print(F"Screen height: {SCREEN_HEIGHT}")
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
   main()
